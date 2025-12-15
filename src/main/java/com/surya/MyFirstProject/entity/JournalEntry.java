edeb53df157040a84b8afcd55581a3943c26b9c8
Surya-0007 <spmittal0007@gmail.com>
2025-12-16 00:14:13 +0530

    Lombok

--- src/main/java/com/surya/MyFirstProject/entity/JournalEntry.java
+++ src/main/java/com/surya/MyFirstProject/entity/JournalEntry.java
@@ -1,5 +1,8 @@
 package com.surya.MyFirstProject.entity;
 
+import jdk.jfr.DataAmount;
+import lombok.Data;
+import lombok.Getter;
 import org.bson.types.ObjectId;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
@@ -8,43 +11,11 @@ import java.time.LocalDateTime;
 import java.util.Date;
 
 @Document(collection = "journal_entries")
+@Data
 public class JournalEntry {
-
     @Id
     private ObjectId id;
     private String title;
     private String content;
     private LocalDateTime date;
-
-    public LocalDateTime getDate() {
-        return date;
-    }
-
-    public void setDate(LocalDateTime date) {
-        this.date = date;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public ObjectId getId() {
-        return id;
-    }
-
-    public void setId(ObjectId id) {
-        this.id = id;
-    }
-
-    public String getContent() {
-        return content;
-    }
-
-    public void setContent(String content) {
-        this.content = content;
-    }
 }
